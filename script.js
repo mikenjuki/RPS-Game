ec0f67285f4559b370fa01d9576529770c669c34
Michael Njuki <mikenjuki@gmail.com>
2022-11-01 11:41:00 +0300

    Initial logic added

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-const startGameBtn = document.getElementById("start-game-btn"); 
+const startGameBtn = document.getElementById("start-game-btn");
+
+const ROCK = "ROCK";
+const PAPER = "PAPER";
+const SCISSORS = "SCISSORS";
+const DEFAULT_USER_CHOICE = ROCK;
+
+const getPlayerChoice = function () {
+    const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}?`, "").toUpperCase();
+
+    if (
+        selection !== ROCK &&
+        selection !== PAPER &&
+        selection !== SCISSORS
+    ) {
+        alert(`Invalid choice, ${DEFAULT_USER_CHOICE} has been selected for you!`);
+        return DEFAULT_USER_CHOICE;
+    }
+    return selection;
+};
